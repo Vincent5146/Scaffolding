e1a1032664aae71e95acb2d38f7dc3ee71a5fcd5
vincent.huang <vincent.huang@ihwic.com>
2023-03-20 11:31:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import Alert from '../components/AlertToastMessage.vue'
+import GoTop from "./components/GoTop.vue"
+</script>
+
+<template>
+  <!-- <Alert /> -->
+  <GoTop />
+  <Router-view />
+</template>
+
+<style lang="scss" scoped>
+// @import './assets/all.scss';
+</style>
